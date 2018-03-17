f158c8ff8f3edf0334b5696519398c73915e3e4c
Carlo Morelli <carlomorelli@gmail.com>
2018-03-17 16:03:59 +0100

    Add parameter to MainMojo

--- src/main/java/com/csoft/MainMojo.java
+++ src/main/java/com/csoft/MainMojo.java
@@ -1,18 +1,18 @@
 package com.csoft;
 
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 
 @Mojo(name = "sayshi")
 public class MainMojo extends AbstractMojo {
 
+    @Parameter(property = "person", defaultValue = "world")
+    private String person;
 
-    public void execute() throws MojoExecutionException, MojoFailureException {
+    public void execute() {
 
-        getLog().info("Hello world");
-
+        getLog().info("Hello " + person);
     }
 }
