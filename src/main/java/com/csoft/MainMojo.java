45d0afd906764f41cbe879dba9e24b2a92fa1232
Carlo Morelli <carlomorelli@gmail.com>
2018-03-20 20:23:35 +0100

    Add list of artifacts

--- src/main/java/com/csoft/MainMojo.java
+++ src/main/java/com/csoft/MainMojo.java
@@ -1,19 +1,22 @@
 package com.csoft;
 
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 
 
 @Mojo(name = "sayshi")
 public class MainMojo extends AbstractMojo {
 
+    @Parameter(defaultValue="${project}", readonly=true, required=true)
+    private MavenProject mavenProject;
+
     @Parameter(property = "person", defaultValue = "world")
     private String person;
 
-    public String getPerson() {
-        return person;
-    }
 
     public void setPerson(String person) {
         this.person = person;
@@ -21,6 +24,10 @@ public class MainMojo extends AbstractMojo {
 
     public void execute() {
         getLog().info("Hello " + person);
+
+        for (Artifact artifact : mavenProject.getArtifacts()) {
+            getLog().info("Found artifact: " + artifact.getGroupId() + ":" + artifact.getArtifactId() + ":" + artifact.getVersion() + ":" + artifact.getScope());
+        }
     }
 
 }
