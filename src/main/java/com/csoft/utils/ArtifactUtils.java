b33b68271abfe7ed771c8715e905a586d2f1462c
Carlo Morelli <carlomorelli@gmail.com>
2023-02-19 17:09:09 +0100

    Remove non-compile/runtime artifact from direct dependencies

--- src/main/java/com/csoft/utils/ArtifactUtils.java
+++ src/main/java/com/csoft/utils/ArtifactUtils.java
@@ -2,13 +2,30 @@ package com.csoft.utils;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.project.MavenProject;
 
 public class ArtifactUtils {
 
-    private ArtifactUtils() {}
+    private ArtifactUtils() {
+    }
+
+    /**
+     * Extracts the set of Direct {@link Artifact} dependencies from a input {@link MavenProject}
+     *
+     * @param mavenProject The input project.
+     * @return Set of dependencies.
+     */
+    public static Set<Artifact> getDirectDependencies(final MavenProject mavenProject) {
+        //NOTE: MavenProject::getDependencyArtifacts automatically returns also diverse scopes such as 'test'.
+        //We filter exclusively on 'runtime' and 'compile'.
+        return mavenProject.getDependencyArtifacts().stream()
+                .filter(artifact -> (artifact.getScope().equalsIgnoreCase("runtime") ||
+                        artifact.getScope().equalsIgnoreCase("compile")))
+                .collect(Collectors.toSet());
+    }
 
     /**
      * Extracts the set of Transitive {@link Artifact} dependencies from a input
@@ -21,7 +38,7 @@ public class ArtifactUtils {
         //NOTE: we have to wrap MavenProject::getArtifacts and ::getDependencyArtifacts output sets into
         //an HashSet to make sure that we Set::removeAll behaves predictibly.
         Set<Artifact> transitiveDependencies = new HashSet<>(mavenProject.getArtifacts());
-        transitiveDependencies.removeAll(new HashSet<>(mavenProject.getDependencyArtifacts()));
+        transitiveDependencies.removeAll(new HashSet<>(getDirectDependencies(mavenProject)));
         return transitiveDependencies;
     }
 
@@ -36,7 +53,7 @@ public class ArtifactUtils {
         //NOTE: we have to wrap MavenProject::getArtifacts and ::getDependencyArtifacts output sets into
         //an HashSet to make sure that we Set::addAll behaves predictibly.
         Set<Artifact> cumulativeDependencies = new HashSet<>(mavenProject.getArtifacts());
-        cumulativeDependencies.addAll(new HashSet<>(mavenProject.getDependencyArtifacts()));
+        cumulativeDependencies.addAll(new HashSet<>(getDirectDependencies(mavenProject)));
         return cumulativeDependencies;
     }
 
